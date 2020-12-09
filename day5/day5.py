5f5e5b6ee96f22b1b3b21bca563d927a1db9a43f
agustinomarruiz <agustinomarruiz@gmail.com>
2020-12-09 12:05:38 -0300

    Solucion parte 2 dia 5

--- day5/day5.py
+++ day5/day5.py
@@ -33,7 +33,17 @@ def find_max_id(boarding_passes):
     return max_id
 
 
+def find_free_seats(boarding_passes):
+    passes_ids = list(map(calculate_boarding_pass_id, boarding_passes))
+    free_spaces = []
+    for id in range(len(passes_ids)):
+        if id not in passes_ids:
+            free_spaces.append(id)
+    return free_spaces
+
+
 with open("puzzleInput.txt", "r") as fp:
     boarding_passes = [line.rstrip() for line in fp]
 
 print(f"Max ID: {find_max_id(boarding_passes)}")
+print(f"Seat ID: {find_free_seats(boarding_passes)}")
