777d736d03ed7d3a6875a6645e36a582e8dee9e9
agustinomarruiz <agustinomarruiz@gmail.com>
2020-12-14 12:34:01 -0300

    AOC day 6 - second half test ok

--- day6/day6.py
+++ day6/day6.py
@@ -1,22 +1,30 @@
 from io import open
 
 
-def calculate_group_declaration(group_declarations):
+def find_group_set_union(group_declarations):
     declarations_union = set()
     for passenger_declaration in group_declarations:
         declarations_union = declarations_union.union(passenger_declaration)
     return len(declarations_union)
 
 
-def sum_of_questions_groups(groups_declarations):
+def find_group_set_intersection(group_declarations):
+    declarations_union = set(group_declarations[0])
+    for passenger_declaration in group_declarations[1:]:
+        declarations_union = declarations_union.intersection(
+            passenger_declaration)
+    return len(declarations_union)
+
+
+def sum_questions(groups_declarations, function_criteria):
     sum_of_questions = 0
     for group_declarations in groups_declarations:
-        sum_of_questions += calculate_group_declaration(group_declarations)
+        sum_of_questions += function_criteria(group_declarations)
     return sum_of_questions
 
 
 groups_declarations = list()
-with open("puzzleInput.txt", "r") as fp:
+with open("puzzleInput_test.txt", "r") as fp:
     group_declarations = list()
     for row in fp:
         if row != "\n":
@@ -29,4 +37,7 @@ with open("puzzleInput.txt", "r") as fp:
         groups_declarations.append(group_declarations)
 
 print(groups_declarations)
-print(sum_of_questions_groups(groups_declarations))
+print(
+    f"Sum of anyone yes questions: {sum_questions(groups_declarations,find_group_set_union)}")
+print(
+    f"Sum of everyone yes questions: {sum_questions(groups_declarations,find_group_set_intersection)}")
