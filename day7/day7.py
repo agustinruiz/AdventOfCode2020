c842143968c5433c75ac0d3fa3301ac9b0307d20
agustinomarruiz <agustinomarruiz@gmail.com>
2020-12-16 10:50:14 -0300

    Day 7 - First part - working with test

--- day7/day7.py
+++ day7/day7.py
@@ -7,7 +7,7 @@ def transform_values_to_touples(values):
         list_values = []
         for detail in values.split(', '):
             list_values.append(
-                (values[0], re.sub('((\ bag\.?$)|(\ bags\.?$))', '', detail)[2:]))
+                (detail[0], re.sub('((\ bag\.?$)|(\ bags\.?$))', '', detail)[2:]))
         return list_values
     return None
 
@@ -17,10 +17,29 @@ def transform_rule_to_dict(rule):
     return {key: transform_values_to_touples(values)}
 
 
+def bag_can_contain(parent, child, rules):
+    value = rules[parent]
+    if not value:
+        return False
+    for count, bag in value:
+        if bag == child:
+            return True
+        return bag_can_contain(bag, child, rules)
+    return
+
+
 rules = list()
 with open("puzzleInput_test.txt", "r") as fp:
     rules = [rule.rstrip() for rule in fp]
 
+rules_dict = dict()
 for rule in rules:
-    print(
-        f"rule: {transform_rule_to_dict(rule)}")
+    rules_dict.update(transform_rule_to_dict(rule))
+print(rules_dict)
+
+count = 0
+for bag in rules_dict:
+    if bag_can_contain(bag, 'shiny gold', rules_dict):
+        count += 1
+
+print(f"Number of bags that can contain shiny gold: {count}")
