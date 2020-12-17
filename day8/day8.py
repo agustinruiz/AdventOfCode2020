2a886a91e44ee015d037bd799e914252d66b0235
agustinomarruiz <agustinomarruiz@gmail.com>
2020-12-17 13:00:37 -0300

    Day 8 - Part 2 - test OK

--- day8/day8.py
+++ day8/day8.py
@@ -2,15 +2,31 @@ from io import open
 from interpreter import Interpreter
 
 instructions = list()
-with open("puzzleInput.txt", "r") as fp:
+with open("puzzleInput_test.txt", "r") as fp:
     instructions = [inst.rstrip() for inst in fp]
 
+
+def perform_execution(interpreter):
+    instractions_executed = [0]
+    for _ in instructions:
+        print(interpreter)
+        if interpreter.perform_instruction():
+            if interpreter.get_instruction_pointer() in instractions_executed:
+                return False
+            instractions_executed.append(interpreter.get_instruction_pointer())
+        else:
+            return True
+
+
 interpreter = Interpreter(0, 0, instructions)
-instractions_executed = [0]
-for instruction in instructions:
-    interpreter.perform_instruction()
-    if interpreter.get_instruction_pointer() in instractions_executed:
-        break
-    instractions_executed.append(interpreter.get_instruction_pointer())
+for index, _ in enumerate(instructions):
+    if interpreter.change_instruction(index):
+        print(instructions)
+        # that change didnt work so returning to the previous value
+        if not perform_execution(interpreter):
+            interpreter.change_instruction(index)
+            interpreter.reset_counters()
+        else:
+            break
 
 print(f"Accumulator: {interpreter.get_accumulator()}")
--- day8/interpreter.py
+++ day8/interpreter.py
@@ -1,4 +1,10 @@
 class Interpreter:
+    # Contructor
+    def __init__(self, accumulator, instruction_pointer, instruction_set):
+        self.accumulator = accumulator
+        self.instruction_pointer = instruction_pointer
+        self.instruction_set = instruction_set
+
     def _perform_acc(self, argument):
         if argument[0] == '+':
             self.accumulator += int(argument[1:])
@@ -21,21 +27,34 @@ class Interpreter:
         'nop': _perform_nop
     }
 
-    # Contructor
-    def __init__(self, accumulator, instruction_pointer, instruction_set):
-        self.accumulator = accumulator
-        self.instruction_pointer = instruction_pointer
-        self.instruction_set = instruction_set
-
     def __str__(self):
         return f"Acumulator: {self.accumulator}, Instruction pointer: {self.instruction_pointer}"
 
     def perform_instruction(self):
-        instruction = self.instruction_set[self.instruction_pointer]
-        self._operations_dict[instruction[:3]](self, instruction.split(" ")[1])
+        if 0 <= self.instruction_pointer < len(self.instruction_set):
+            instruction = self.instruction_set[self.instruction_pointer]
+            self._operations_dict[instruction[:3]](
+                self, instruction.split(" ")[1])
+            return True
+        return False
 
     def get_accumulator(self):
         return self.accumulator
 
     def get_instruction_pointer(self):
         return self.instruction_pointer
+
+    def change_instruction(self, index):
+        if self.instruction_set[index][:3] == 'jmp':
+            self.instruction_set[index] = 'nop' + \
+                self.instruction_set[index][3:]
+            return True
+        if self.instruction_set[index][:3] == 'nop':
+            self.instruction_set[index] = 'jmp' + \
+                self.instruction_set[index][3:]
+            return True
+        return False
+
+    def reset_counters(self):
+        self.accumulator = 0
+        self.instruction_pointer = 0
