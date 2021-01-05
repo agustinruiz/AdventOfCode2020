1c3c5a8bc56486cd280cc2a3a78bf7d6c4ef5e28
agustinomarruiz <agustinomarruiz@gmail.com>
2021-01-05 10:52:40 -0300

    Day 9 - Second Part - Working with test values

--- day9/day9.py
+++ day9/day9.py
@@ -1,7 +1,7 @@
 from io import open
 
 lines = list()
-with open("puzzleInput.txt", "r") as fp:
+with open("puzzleInput_test.txt", "r") as fp:
     lines = [line for line in fp]
 
 
@@ -16,7 +16,26 @@ def is_sum_of(number, number_list):
 def find_first_wrong(preamble, number_list):
     for index, number in enumerate(number_list[preamble:]):
         if not is_sum_of(number, number_list[index:index+preamble]):
-            return number
+            return (index, int(number))
 
 
-print(f"First number without the property: {find_first_wrong(25,lines)}")
+def find_contiguous_set(index, number, number_list):
+    for j in range(index):
+        contiguous_sum = 0
+        sum_set = set()
+        for list_num in number_list[j:index]:
+            sum_set.add(int(list_num))
+            contiguous_sum += int(list_num)
+            if contiguous_sum == number:
+                return sorted(sum_set, reverse=False)
+            elif contiguous_sum > number:
+                break
+
+
+first_wrong = find_first_wrong(5, lines)
+print(f"First number without the property: {first_wrong}")
+
+contiguous_set = find_contiguous_set(first_wrong[0], first_wrong[1], lines)
+print(f"First number without the property: {contiguous_set}")
+
+print(f"Sum of contiguous set: {contiguous_set[0] + contiguous_set[-1]}")
